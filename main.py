e8cce56e5f1a0d4ed1ddf0b8af2d89f9ca3d26bf
Byxs20 <97766819@qq.com>
2022-08-16 17:58:52 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,69 @@
+import sys
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidgetItem, QMessageBox
+from UI.ui import Ui_MainWindow
+
+
+class Main(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+        self.setWindowIcon(QIcon("Logo.ico")) # 设置软件图标
+        self.setFixedSize(self.width(), self.height()) # 禁止窗口最大化
+        self.tableWidget.verticalHeader().setVisible(False) # 表格不显示表头
+
+        # 信号
+        self.pushButton.clicked.connect(self.show_info)
+
+    def get_text(self):
+        if (text := self.plainTextEdit.toPlainText()) != "":
+            return text
+        QMessageBox.information(self, "温馨提示", "您输入的内容为空!", QMessageBox.Yes)
+
+    def get_dic(self, text):
+        return {i: 0 for i in text}
+
+    def get_frequency(self):
+        text = self.get_text()
+        if text is not None:
+            dic = self.get_dic(text)
+
+            for character in text:
+                if character in dic:
+                    dic[character] += 1
+
+            # 从大到小排序
+            sort_info = sorted(dic.items(), key=lambda i:i[1], reverse=True) # [('e', 39915), ... ('z', 264)]
+            
+            # 获取总字母数量
+            number = sum(num for _, num in sort_info)
+
+            # 添加频率
+            edit_info = ""
+            table_info = []
+            for index, (character, num) in enumerate(sort_info):
+                if num != 0:
+                    frequency = round((num / number) * 100, 4)
+                    edit_info += character
+                    table_info.append((index + 1, character, num, frequency))
+            return edit_info, table_info
+
+    def show_info(self):
+        if (ret := self.get_frequency()) is not None:
+            edit_info, table_info = ret
+            self.tableWidget.setRowCount(len(table_info))
+            for x in range(self.tableWidget.rowCount()):
+                for y in range(self.tableWidget.columnCount()):
+                    item = QTableWidgetItem(str(table_info[x][y]))
+                    item.setTextAlignment(Qt.AlignHCenter | Qt.AlignVCenter)
+                    self.tableWidget.setItem(x, y, item)
+
+            self.lineEdit.setText(edit_info)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    ui = Main()
+    ui.show()
+    sys.exit(app.exec_())
