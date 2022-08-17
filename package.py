6b8dfa0057f6599a1ba4fc3d1da90793e1c11bd0
Byxs20 <97766819@qq.com>
2022-08-17 22:15:00 +0800

    \	modified:   package.py

--- package.py
+++ package.py
@@ -1,8 +1,16 @@
 import os
 
-# build
-os.system(r"pyinstaller.exe -wF --distpath .\bin\build --workpath .\bin\ -i .\images\Logo.ico .\main.py -n 字频频率统计")
 
+# path
+dist_path = "./bin/build"
+work_path = "./bin"
+ico_ptah = "./images/Logo.ico"
+upx_path = r"D:\Byxs20\Downloads\Compressed\upx-3.96-win64"
+
+# build
+os.system(f"pyinstaller.exe -wF -n 字频频率统计 --distpath {dist_path} --workpath {work_path} -i {ico_ptah} --upx-dir {upx_path} .\main.py")
+
+# copy ico file
 if not os.path.exists("./bin/build/images"):
     os.makedirs("./bin/build/images")
 os.system(r"copy .\images\Logo.ico .\bin\build\images\Logo.ico")
