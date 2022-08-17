23c555f3ad3e34e1576ed323110f659df22a4764
Byxs20 <97766819@qq.com>
2022-08-17 13:10:16 +0800

    update package.py

--- package.py
+++ package.py
@@ -1,5 +1,8 @@
 import os
 
+# build
 os.system(r"pyinstaller.exe -wF --distpath .\bin\build --workpath .\bin\ -i .\images\Logo.ico .\main.py -n 字频频率统计")
-os.makedirs("./bin/build/images")
+
+if not os.path.exists("./bin/build/images"):
+    os.makedirs("./bin/build/images")
 os.system(r"copy .\images\Logo.ico .\bin\build\images\Logo.ico")
